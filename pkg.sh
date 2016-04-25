661c638912e732b58ad916b80c46c1ce632e6c8a
Sergi Àlvarez <pancake@nowsecure.com>
2016-04-26 01:37:13 +0200

    Enhancements for the OSX package

--- pkg.sh
+++ pkg.sh
@@ -6,25 +6,42 @@
 # to uninstall:
 # sudo pkgutil --forget com.nowsecure.fsmon
 
-SRC=/tmp/r2osx
+DESTDIR=/tmp/fsmon_pkg
 PREFIX=/usr/local
-DST="$(pwd)/fsmon.unpkg"
+PKGDIR="$(pwd)/fsmon.unpkg"
 [ -z "${VERSION}" ] && VERSION=1.1
 [ -z "${MAKE}" ] && MAKE=make
 VERSION=1.1
 
-rm -rf "${SRC}"
+rm -rf "${DESTDIR}"
 ${MAKE} clean
 make || exit 1
-${MAKE} install PREFIX="${PREFIX}" DESTDIR=${SRC} || exit 1
-mkdir -p "${DST}"
-if [ -d "${SRC}" ]; then
+${MAKE} install PREFIX="${PREFIX}" DESTDIR=${DESTDIR} || exit 1
+rm -rf "${PKGDIR}"
+mkdir -p "${PKGDIR}"
+
+cat > "${PKGDIR}/PackageInfo" << EOF
+<?xml version="1.0" encoding="utf-8" standalone="no"?>
+<pkg-info overwrite-permissions="true" relocatable="false" identifier="com.nowsecure.fsmon" postinstall-action="none" version="0" format-version="2" generator-version="InstallCmds-237 (11E53)" auth="root">
+    <payload numberOfFiles="2" installKBytes="31"/>
+    <bundle-version/>
+    <upgrade-bundle/>
+    <update-bundle/>
+    <atomic-update-bundle/>
+    <strict-identifier/>
+    <relocate/>
+</pkg-info>
+EOF
+
+if [ -d "${DESTDIR}" ]; then
 	(
-		cd "${SRC}" && \
-		find . | cpio -o --format odc | gzip -c > "${DST}/Payload"
+		cd "${DESTDIR}" && \
+		find . | cpio -o --format odc | gzip -c > "${PKGDIR}/Payload"
 	)
-	mkbom "${SRC}" "${DST}/Bom"
-	pkgutil --flatten "${DST}" "fsmon-${VERSION}.pkg"
+	mkbom "${DESTDIR}" "${PKGDIR}/Bom"
+	pkgutil --flatten "${PKGDIR}" "fsmon-${VERSION}.pkg"
+	rm -rf "${DESTDIR}"
+	rm -rf "${PKGDIR}"
 else
 	echo "Failed install. DESTDIR is empty"
 	exit 1
